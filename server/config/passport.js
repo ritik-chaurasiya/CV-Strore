11894a769ac0cf818063ba2db37445437b3d8d8e
Ritik Chaurasiya <ritikchaurasiya2005@gmail.com>
2026-06-18 18:23:02 +0530

    Add vercel rewrites

--- server/config/passport.js
+++ server/config/passport.js
@@ -12,7 +12,7 @@ passport.use(
             clientSecret:
                 process.env.GOOGLE_CLIENT_SECRET,
             callbackURL:
-                "http://localhost:5000/api/auth/google/callback",
+                "https://cv-strore.onrender.com/api/auth/google/callback",
         },
 
         async (
