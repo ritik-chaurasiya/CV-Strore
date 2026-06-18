e52987eb6367af04e5a4c9752d51f3c3cd2006b1
Ritik Chaurasiya <ritikchaurasiya2005@gmail.com>
2026-06-18 17:30:36 +0530

    Update CORS

--- server/server.js
+++ server/server.js
@@ -26,9 +26,10 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://cv-strore.vercel.app/",
+      "https://cv-strore.vercel.app",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 app.use(express.json());
